18fef73b77838e7b7fa5507c4c5f78c31a5d0201
Elizabeth <107285224+legndarycryst4@users.noreply.github.com>
2025-12-11 00:13:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ app = Flask(__name__, template_folder='.')
 def home():
     return render_template('index.html')
 
+@app.route('/styles.css')
+def home():
+    return render_template('./styles.css')
+
 @app.route('/girl-power')
 def girl_power_index():
-    return render_template('Girl-Power/index.html')
+    return render_template('./Girl-Power/index.html')
+    
 @app.route('/girl-power/media')
 def girl_power_media():
-    return render_template('Girl-Power/gallery.html')
+    return render_template('./Girl-Power/gallery.html')
+    
 @app.route('/girl-power/<path:filename>')
 def serve_girl_power_files(filename):
     return send_from_directory('Girl-Power', filename)
