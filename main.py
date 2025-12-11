2ea175903145c947e7cd1abdd5657a556ab441cb
Elizabeth <107285224+legndarycryst4@users.noreply.github.com>
2025-12-11 00:27:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/styles.css')
-def home():
-    return render_template('./styles.css')
-
 @app.route('/girl-power')
 def girl_power_index():
-    return render_template('./Girl-Power/index.html')
-    
+    return render_template('Girl-Power/index.html')
+
 @app.route('/girl-power/media')
 def girl_power_media():
-    return render_template('./Girl-Power/gallery.html')
-    
-@app.route('/girl-power/<path:filename>')
-def serve_girl_power_files(filename):
-    return send_from_directory('Girl-Power', filename)
+    return render_template('Girl-Power/gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
